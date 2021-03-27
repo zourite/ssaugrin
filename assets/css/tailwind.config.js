ae1437ffadf2c2468ba5c9783bea46ea0ed198cb
zourite <ssaugrin@gmail.com>
2021-03-27 22:58:58 +0400

    [FIX] Page d'acceuil

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
         options: {
           whitelist: [],
         }
-    },
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1280px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1600px',
+        // => @media (min-width: 1280px) { ... }
+      },
       extend: {
         colors: {
           'drk-green-octo': '#114b5f',
