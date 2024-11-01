8933fb7890351d7e7fe31f3416dadef14414da25
Sonia Saugrin <me@ssaugrin.fr>
2024-11-01 23:22:49 +0400

    improve: use iconify

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   plugins: [
     require('postcss-nested-ancestors'),
-    require('postcss-viewport-unit-fallback'),
     require('postcss-nested'),
     require('rfs'),
     require('postcss-import')({
       root: './assets/css',
     }),
     require('tailwindcss'),
+    require('postcss-viewport-unit-fallback'),
     require('autoprefixer')({
       overrideBrowserslist: ['>1%']
     })
