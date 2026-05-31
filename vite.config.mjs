21e0f461b6f6640be3df87831e753c3329b17d2a
zourite <ssaugrin@gmail.com>
2026-05-31 18:52:10 +0400

    Add hero to home

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,10 +8,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(process.cwd(), "assets/css/main.css"),
+        app: resolve(process.cwd(), "assets/js/app.js"),
       },
       output: {
         assetFileNames: "assets/[name][extname]",
+        entryFileNames: "assets/[name].js",
       },
     },
+  },
+  server: {
+    origin: "http://localhost:5173",
   },
 });
